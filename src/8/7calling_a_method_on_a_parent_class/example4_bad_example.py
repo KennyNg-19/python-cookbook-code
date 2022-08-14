33b285b0c12f7d0936ea1df1bfaa7349a2f72d2f
kenny_wu <wuyuhao2019@126.com>
2022-08-14 22:37:47 +0800

    super的一些特性

--- src/8/7calling_a_method_on_a_parent_class/example4_bad_example.py
+++ src/8/7calling_a_method_on_a_parent_class/example4_bad_example.py
@@ -1,25 +1,41 @@
+'''
+Author: kenny_wu
+Date: 2022-08-08 13:02:22
+LastEditors: kenny_wu
+LastEditTime: 2022-08-14 22:35:44
+Description: 
+'''
 # Tricky initialization problem involving multiple inheritance.
 # Does NOT use super()
 
+
 class Base:
+
     def __init__(self):
         print('Base.__init__')
 
+
 class A(Base):
+
     def __init__(self):
         Base.__init__(self)
         print('A.__init__')
 
+
 class B(Base):
+
     def __init__(self):
         Base.__init__(self)
         print('B.__init__')
 
-class C(A,B):
+
+class C(A, B):
+
     def __init__(self):
         A.__init__(self)
         B.__init__(self)
         print('C.__init__')
+
 
 if __name__ == '__main__':
     # Please observe double call of Base.__init__
--- src/8/7calling_a_method_on_a_parent_class/example5_good_ex.py
+++ src/8/7calling_a_method_on_a_parent_class/example5_good_ex.py
@@ -1,24 +1,40 @@
+'''
+Author: kenny_wu
+Date: 2022-08-08 13:02:22
+LastEditors: kenny_wu
+LastEditTime: 2022-08-14 22:35:59
+Description: 
+'''
 # Tricky initialization problem involving multiple inheritance.
 # Uses super()
 
+
 class Base:
+
     def __init__(self):
         print('Base.__init__')
 
+
 class A(Base):
+
     def __init__(self):
         super().__init__()
         print('A.__init__')
 
+
 class B(Base):
+
     def __init__(self):
         super().__init__()
         print('B.__init__')
 
-class C(A,B):
+
+class C(A, B):
+
     def __init__(self):
-        super().__init__()     # Only one call to super() here
+        super().__init__()  # Only one call to super() here
         print('C.__init__')
+
 
 if __name__ == '__main__':
     # Observe that each class initialized only once
